e3ecf8fe0133904b5fc0a40e5ee4550280e996ef
Tobias Frommhund <t.frommhund@get.systems>
2023-05-02 20:03:19 +0200

    Update fish alias and env variables

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,10 +2,43 @@ alias icat='kitty +kitten icat'
 
 alias mirror=' sudo reflector --country AT --latest 50 --sort rate --save /etc/pacman.d/mirrorlist'
 
-alias ls='exa --icons -a'
+alias ls='exa --icons --all --group-directories-first'
 alias ps='procs'
 alias grep='rg'
 alias vim='nvim'
+alias cat='bat -p'
 
+alias yeet='yay -Rns'
+
+
+set -x XDG_CONFIG_HOME "$HOME/.config"
+set -x XDG_DATA_HOME "$HOME/.local/share"
+set -x XDG_STATE_HOME "$HOME/.local/state"
+set -x XDG_CACHE_HOME "$HOME/.cache"
+
+set -x ANDROID_HOME "$XDG_DATA_HOME"/android
+set -x GRADLE_USER_HOME "$XDG_DATA_HOME"/gradle
+set -x PASSWORD_STORE_DIR "$XDG_DATA_HOME"/password-store
+set -x GOPATH "$XDG_DATA_HOME"/go
+set -x GNUPGHOME "$XDG_DATA_HOME"/gnupg
+set -x CARGO_HOME "$XDG_DATA_HOME"/cargo
+set -x WINEPREFIX "$XDG_DATA_HOME"/wineprefixes/default
+set -x TERMINFO "$XDG_DATA_HOME"/terminfo
+set -x TERMINFO_DIRS "$XDG_DATA_HOME"/terminfo:/usr/share/terminfo
+
+set -x XINITRC "$XDG_CONFIG_HOME"/X11/xinitrc
+set -x WGETRC "$XDG_CONFIG_HOME"/wget/wgetrc
+set -x ZDOTDIR "$XDG_CONFIG_HOME"/zsh
+set -x GTK2_RC_FILES "$XDG_CONFIG_HOME"/gtk-2.0/gtkrc
+
+set -x HISTFILE "$XDG_STATE_HOME"/bash/history
+
+set -x XAUTHORITY "$XDG_RUNTIME_DIR"/Xauthority
+
+set -x MANPAGER "sh -c 'col -bx | bat -l man -p'"
+set -x LESSHISTFILE -
+
+
+wal -R -e -n -q
 
 starship init fish | source
