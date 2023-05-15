fb66a038df79c19508c5d795fb87789b3174b98b
FromWau <tobias.frommhund@aon.at>
2023-05-15 10:34:17 +0200

    cleanup and add set env variables

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,16 +1,17 @@
+# Alias
 alias icat='kitty +kitten icat'
-
-alias mirror=' sudo reflector --country AT --latest 50 --sort rate --save /etc/pacman.d/mirrorlist'
-
+alias mirror='sudo reflector --country AT --latest 50 --sort rate --save /etc/pacman.d/mirrorlist'
 alias ls='exa --icons --all --group-directories-first'
 alias ps='procs'
 alias grep='rg'
 alias vim='nvim'
 alias cat='bat -p'
-
 alias yeet='yay -Rns'
 
+alias bed='hyprctl keyword monitor HDMI-A-1,3840x2160@60,0x0,2'
+alias desk='hyprctl keyword monitor HDMI-A-1,3840x2160@60,0x0,1'
 
+# Env variables
 set -x XDG_CONFIG_HOME "$HOME/.config"
 set -x XDG_DATA_HOME "$HOME/.local/share"
 set -x XDG_STATE_HOME "$HOME/.local/state"
@@ -38,7 +39,10 @@ set -x XAUTHORITY "$XDG_RUNTIME_DIR"/Xauthority
 set -x MANPAGER "sh -c 'col -bx | bat -l man -p'"
 set -x LESSHISTFILE -
 
+# Append dirs to path
+fish_add_path ~/.local/bin
 
-wal -R -e -n -q
+# wal -R -e -n -q
 
+# start starship
 starship init fish | source
