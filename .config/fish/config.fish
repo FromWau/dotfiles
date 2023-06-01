e25d037693aa6396c1b83867ec87b909c4c6d0c3
FromWau <tobias.frommhund@aon.at>
2023-06-02 00:57:13 +0200

    updated fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,20 +1,3 @@
-# Alias
-alias icat='kitty +kitten icat'
-alias mirror='sudo reflector --country AT --latest 50 --sort rate --save /etc/pacman.d/mirrorlist'
-alias ls='exa --icons --all --group-directories-first'
-alias ps='procs'
-alias grep='rg'
-alias fzf="fzf -d '|' --cycle -i"
-alias vim='nvim'
-alias v='nvim'
-alias cat='bat -p --color always'
-alias yeet='yay -Rns'
-
-alias bed='hyprctl keyword monitor HDMI-A-1,3840x2160@60,0x0,2'
-alias desk='hyprctl keyword monitor HDMI-A-1,3840x2160@60,0x0,1'
-
-alias reload='source ~/.config/fish/config.fish'
-
 # Env variables
 set -x XDG_CONFIG_HOME "$HOME/.config"
 set -x XDG_DATA_HOME "$HOME/.local/share"
@@ -44,7 +27,7 @@ set -x XAUTHORITY "$XDG_RUNTIME_DIR"/Xauthority
 
 set -x MANPAGER "sh -c 'col -bx | bat -l man -p'"
 set -x LESSHISTFILE -
-set -x FZF_DEFAULT_COMMAND "command fd --type f --strip-cwd-prefix --hidden --follow --exclude .git"
+set -x FZF_DEFAULT_COMMAND "fd --type f --strip-cwd-prefix --hidden --follow --exclude .git"
 
 fish_vi_key_bindings insert
 
@@ -55,20 +38,41 @@ fish_add_path ~/.local/bin
 starship init fish | source
 
 zoxide init fish | source
-alias cd='z'
 
+
+# abbr
+abbr icat 'kitty +kitten icat'
+abbr mirror 'sudo reflector --country AT --latest 50 --sort rate --save /etc/pacman.d/mirrorlist'
+abbr ls 'exa --icons --all --group-directories-first --color always'
+abbr ps procs
+abbr grep rg
+abbr fzf 'fzf -d "|" --cycle -i'
+abbr cd z
+abbr vim nvim
+abbr v nvim
+abbr cat 'bat -p --color always'
+abbr yeet 'yay -Rns'
+abbr bed 'hyprctl keyword monitor HDMI-A-1,3840x2160@60,0x0,2'
+abbr desk 'hyprctl keyword monitor HDMI-A-1,3840x2160@60,0x0,1'
+abbr reload 'source ~/.config/fish/config.fish'
+
+
+# functions
 function fdir -d "Jump to selected zoxide dir"
-    set -l selected_dir $(zoxide query -l | command fzf --preview "ls -lah {}" --cycle -i --exact --prompt "z into dir: ")
+    set -l selected_dir $(zoxide query -l | fzf --preview "ls -lah {}" --cycle -i -d "|" --exact --prompt "z into dir: ")
     z $selected_dir
     echo "Jumped to $selected_dir"
     ls
 end
 
-function rm-pics -d "Delete muliple selected pictures"
-    set -l selected_files $(fzf-cat -m -q '.png | .jpg | .jpeg Pictures')
-    rm "$selected_files" 2>/dev/null
+function mcd -d "Creates and enters dir"
+    mkdir -p -- $argv && z $argv
 end
 
+function rm-fzf -d "Delete muliple selected files"
+    fzf-previewer -m | xargs rm
+end
 
-alias ff='command fzf --preview "bat --color always {}" --cycle -i --bind "enter:execute(nvim {1} < /dev/tty)" --exact --prompt "Open in nvim: "'
-alias fp='fzf-cat'
+function ff -d "Search for files and open in nvim"
+    fzf-previewer --cycle -i -d "|" --bind "enter:execute(nvim {1} < /dev/tty)" --exact --prompt "Open in nvim: "
+end
