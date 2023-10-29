0176563cea32e5da98683c3ad472c5c750761b18
FromWau <tobias.frommhund@aon.at>
2023-10-29 23:25:00 +0100

    nvim: better copilot

--- .config/nvim/lua/plugins/copilot.lua
+++ .config/nvim/lua/plugins/copilot.lua
@@ -1,11 +1,25 @@
 return {
 	"zbirenbaum/copilot.lua",
 	cmd = "Copilot",
+	build = ":Copilot auth",
 	event = "InsertEnter",
+	dependencies = "zbirenbaum/copilot-cmp",
 	config = function()
 		require("copilot").setup({
 			suggestion = {
+				enabled = true,
 				auto_trigger = true,
+			},
+			panel = { enabled = false },
+			filetypes = {
+				["*"] = true,
+				help = true,
+			},
+		})
+
+		require("copilot_cmp").setup({
+			formatters = {
+				insert_text = require("copilot_cmp.format").remove_existing,
 			},
 		})
 	end,
