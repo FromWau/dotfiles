586d81eac61c2515c3e260ec4212a510e7492f5a
FromWau <tobias.frommhund@aon.at>
2023-10-29 20:24:12 +0100

    nvim: added inc-rename

--- .config/nvim/lua/plugins/noice.lua
+++ .config/nvim/lua/plugins/noice.lua
@@ -2,13 +2,18 @@ return {
     "folke/noice.nvim",
     event = "VeryLazy",
     opts = {},
+    config = function ()
+        require("noice").setup {
+            presets = { inc_rename = true }
+        }
+    end,
     dependencies = {
         "MunifTanjim/nui.nvim",
         {
             "rcarriga/nvim-notify",
             config = function()
                 require("notify").setup({
-                    background_color = "#000000"
+                    background_colour = "#000000"
                 })
             end
         }
--- .config/nvim/lua/plugins/nvim-lspconfig.lua
+++ .config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -18,7 +18,7 @@ local config = function()
         vim.keymap.set("n", "<leader>gd", "<cmd>Lspsaga peek_definition<CR>", opts) -- peak definition
         vim.keymap.set("n", "<leader>gD", "<cmd>Lspsaga goto_definition<CR>", opts) -- go to definition
         vim.keymap.set("n", "<leader>ca", "<cmd>Lspsaga code_action<CR>", opts) -- see available code actions
-        vim.keymap.set("n", "<leader>rn", "<cmd>Lspsaga rename<CR>", opts) -- smart rename
+        -- vim.keymap.set("n", "<leader>rn", "<cmd>Lspsaga rename<CR>", opts) -- smart rename
         vim.keymap.set("n", "<leader>D", "<cmd>Lspsaga show_line_diagnostics<CR>", opts) -- show  diagnostics for line
         vim.keymap.set("n", "<leader>d", "<cmd>Lspsaga show_cursor_diagnostics<CR>", opts) -- show diagnostics for cursor
         vim.keymap.set("n", "<leader>pd", "<cmd>Lspsaga diagnostic_jump_prev<CR>", opts) -- jump to prev diagnostic in buffer
