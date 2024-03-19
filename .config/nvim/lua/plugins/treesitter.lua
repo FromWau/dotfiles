de3c3642052a420383c33d1f1afda013f42418df
FromWau <tobias.frommhund@aon.at>
2024-03-19 15:05:34 +0100

    wip

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -3,10 +3,39 @@ return { -- Highlight, edit, and navigate code
     dependencies = "nvim-treesitter/nvim-treesitter-textobjects",
     build = ":TSUpdate",
     cmd = { "TSUpdateSync", "TSUpdate", "TSInstall" },
-    opts = {
-        ensure_installed = { "bash", "lua", "toml" },
-        auto_install = true,
-        highlight = { enable = true },
-        indent = { enable = true },
-    },
+    config = function()
+        require("nvim-treesitter.configs").setup {
+            ensure_installed = { "bash", "lua", "toml", "json" },
+            auto_install = true,
+            highlight = { enable = true },
+            indent = { enable = true },
+            textobjects = {
+                select = {
+                    enable = true,
+                    lookahead = true,
+                    keymaps = {
+                        -- You can use the capture groups defined in textobjects.scm
+                        ["af"] = "@function.outer",
+                        ["if"] = "@function.inner",
+                        ["ac"] = "@class.outer",
+                        -- You can optionally set descriptions to the mappings (used in the desc parameter of
+                        -- nvim_buf_set_keymap) which plugins like which-key display
+                        ["ic"] = { query = "@class.inner", desc = "Select inner part of a class region" },
+                        -- You can also use captures from other query groups like `locals.scm`
+                        ["as"] = { query = "@scope", query_group = "locals", desc = "Select language scope" },
+                    },
+                    include_surrounding_whitespace = true,
+                },
+                swap = {
+                    enable = true,
+                    swap_next = {
+                        ["<leader>a"] = "@parameter.inner",
+                    },
+                    swap_previous = {
+                        ["<leader>A"] = "@parameter.inner",
+                    },
+                },
+            },
+        }
+    end,
 }
