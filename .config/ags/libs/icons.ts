c075616fe1ca24cd5120adc563b34314c36a09a5
FromWau <tobias.frommhund@aon.at>
2024-08-01 01:00:47 +0200

    ags: wip

--- .config/ags/libs/icons.ts
+++ .config/ags/libs/icons.ts
@@ -44,5 +44,142 @@ export default {
             searching: "find-location-symbolic",
             edit: "edit-symbolic",
         },
+        codes: {
+            0: {
+                icon: "weather-clear-symbolic",
+                text: "Clear sky",
+            },
+            1: {
+                icon: "weather-few-clouds-symbolic",
+                text: "Mainly clouds",
+            },
+            2: {
+                icon: "weather-many-clouds-symbolic",
+                text: "Partly clouds",
+            },
+            3: {
+                icon: "weather-overcast-symbolic",
+                text: "Overcast",
+            },
+            45: {
+                icon: "weather-fog-symbolic",
+                text: "Fog",
+            },
+            48: {
+                // no icon
+                icon: "weather-rime-fog-symbolic",
+                text: "Depositing rime fog",
+            },
+            51: {
+                // no icon
+                icon: "weather-light-rain-symbolic",
+                text: "Light drizzle",
+            },
+            53: {
+                // no icon
+                icon: "weather-moderate-rain-symbolic",
+                text: "Moderate drizzle",
+            },
+            55: {
+                // no icon
+                icon: "weather-heavy-rain-symbolic",
+                text: "Dense drizzle",
+            },
+            56: {
+                // no icon
+                icon: "weather-light-showers-symbolic",
+                text: "Light freezing drizzle",
+            },
+            57: {
+                // no icon
+                icon: "weather-moderate-showers-symbolic",
+                text: "Dense freezing drizzle",
+            },
+            61: {
+                // no icon
+                icon: "weather-slight-rain-symbolic",
+                text: "Slight rain",
+            },
+            63: {
+                // no icon
+                icon: "weather-moderate-rain-symbolic",
+                text: "Moderate rain",
+            },
+            65: {
+                // no icon
+                icon: "weather-heavy-rain-symbolic",
+                text: "Heavy rain",
+            },
+            66: {
+                // no icon
+                icon: "weather-light-freezing-rain-symbolic",
+                text: "Freezing light rain",
+            },
+            67: {
+                // no icon
+                icon: "weather-heavy-freezing-rain-symbolic",
+                text: "Freezing heavy rain",
+            },
+            71: {
+                // no icon
+                icon: "weather-slight-snow-fall-symbolic",
+                text: "Slight snow fall",
+            },
+            73: {
+                // no icon
+                icon: "weather-moderate-snow-fall-symbolic",
+                text: "Moderate snow fall",
+            },
+            75: {
+                // no icon
+                icon: "weather-heavy-snow-fall-symbolic",
+                text: "Heavy snow fall",
+            },
+            77: {
+                // no icon
+                icon: "weather-snow-grains-symbolic",
+                text: "Snow grains",
+            },
+            80: {
+                // no icon
+                icon: "weather-slight-rain-showers-symbolic",
+                text: "Slight rain showers",
+            },
+            81: {
+                // no icon
+                icon: "weather-moderate-rain-showers-symbolic",
+                text: "Moderate rain showers",
+            },
+            82: {
+                // no icon
+                icon: "weather-violent-rain-showers-symbolic",
+                text: "Violent rain showers",
+            },
+            85: {
+                // no icon
+                icon: "weather-slight-snow-showers-symbolic",
+                text: "Slight snow showers",
+            },
+            86: {
+                // no icon
+                icon: "weather-heavy-snow-showers-symbolic",
+                text: "Heavy snow showers",
+            },
+            95: {
+                // no icon
+                icon: "weather-thunderstorm-symbolic",
+                text: "Thunderstorm",
+            },
+            96: {
+                // no icon
+                icon: "weather-thunderstorm-light-hail-symbolic",
+                text: "Thunderstorm with light hail",
+            },
+            99: {
+                // no icon
+                icons: "weather-thunderstorm-heavy-hail-symbolic",
+                text: "Thunderstorm with heavy hail",
+            },
+        },
     },
 }
