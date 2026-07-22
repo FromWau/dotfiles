b74beccc630c30d356907c7d3e3892a3fc11e512
FromWau <tobias.frommhund@aon.at>
2026-07-22 14:02:50 +0200

    walker: searchable keybinds help menu

---
+++ .config/elephant/menus/keybinds.lua
@@ -0,0 +1,28 @@
+Name = "keybinds"
+NamePretty = "Keybinds"
+Description = "Keyboard shortcut help for walker and hyprland"
+Icon = "input-keyboard"
+SearchName = true
+Keywords = { "keys", "keybinds", "shortcuts", "hotkeys", "help", "whichkey" }
+FixedOrder = true
+
+function GetEntries()
+    local entries = {}
+
+    local handle = io.popen("keybinds-list")
+    if handle then
+        for line in handle:lines() do
+            local source, combo, desc = line:match("^(.-)\t(.-)\t(.*)$")
+            if combo then
+                table.insert(entries, {
+                    Text = combo,
+                    Subtext = source .. " · " .. desc,
+                    Icon = "input-keyboard",
+                })
+            end
+        end
+        handle:close()
+    end
+
+    return entries
+end
